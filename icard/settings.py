592bf88b61da75259f681791eb74d9335cf87bbf
marlonmedina6827 <marlonmedinachavez@gmail.com>
2024-01-20 11:30:31 -0500

    BACKEND USUARIO

--- icard/settings.py
+++ icard/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'drf_yasg',
     'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#USERS
+
+AUTH_USER_MODEL = 'users.User'
